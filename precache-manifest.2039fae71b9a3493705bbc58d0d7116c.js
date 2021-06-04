72bd2ee854063802eeb00f79b8b5a6fe15324e20
Sujan Byanjankar <byanjankarsujan@gmail.com>
2021-06-04 17:21:34 +0545

    Updates

---
+++ precache-manifest.2039fae71b9a3493705bbc58d0d7116c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9d22320d8791d7081f0279bc4ab3fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f46a201dfa5441bab5ce",
+    "url": "./static/css/main.1355b07b.chunk.css"
+  },
+  {
+    "revision": "6a2a8655e0fe8c300923",
+    "url": "./static/js/2.1b9cf849.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1b9cf849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46a201dfa5441bab5ce",
+    "url": "./static/js/main.c99f79ce.chunk.js"
+  },
+  {
+    "revision": "9678c45ef6ff07781d09",
+    "url": "./static/js/runtime-main.a4814a41.js"
+  }
+]);
